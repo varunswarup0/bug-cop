2b90c42730ea22322624ec14296cd1e97bf171a9
Varun Swarup <varunswarup0@gmail.com>
2020-03-24 20:52:13 +0530

    Added actionTypes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import store from './store';
+import * as actions from './actionTypes';
+
+// state = reducer(state, action);
+// notfiy the subscribers
 
 const unsubscribe = store.subscribe(() => {
 	console.log('Store Changed', store.getState());
 });
 
 store.dispatch({
-	type: 'bugAdded',
+	type: actions.BUG_ADDED,
 	payload: {
 		description: 'Bug1'
 	}
 });
 
-unsubscribe();
+// unsubscribe();
 
 store.dispatch({
-	type: 'bugRemoved',
+	type: actions.BUG_REMOVED,
 	payload: {
 		id: 1
 	}
