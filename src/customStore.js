115bd0b84d102c97bc778f4486967a8ad65fe4c3
Varun Swarup <varunswarup0@gmail.com>
2020-03-24 23:10:34 +0530

    Added dispatch & subscribe

--- src/customStore.js
+++ src/customStore.js
@@ -1,11 +1,23 @@
-function createStore() {
+import reducer from './reducer';
+
+function createStore(reducer) {
 	let state;
+	let listeners = [];
+
+	function subscribe(listener) {
+		listeners.push(listener);
+	}
+
+	function dispatch(action) {
+		state = reducer(state, action);
+		for (let i = 0; i < listeners.length; i++) listeners[i]();
+	}
 
 	function getState() {
 		return state;
 	}
 
-	return { getState };
+	return { getState, dispatch, subscribe };
 }
 
-export default createStore();
+export default createStore(reducer);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import store from './customStore';
-store.state = 1;
-console.log(store);
+import * as actions from './actions';
+
+store.subscribe(() => {
+	console.log('Store Changed!');
+});
+
+store.dispatch(actions.bugAdded('Bug 1'));
+
+console.log(store.getState());
