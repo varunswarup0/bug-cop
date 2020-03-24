d07549d81323853148509f6f89f963021336c3fc
Varun Swarup <varunswarup0@gmail.com>
2020-03-24 10:24:18 +0530

    Added compose, pipe & currying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp';
+
+let input = '  JavaScript  ';
+let output = '<div>' + input.trim() + '</div';
+
+const trim = str => str.trim();
+
+// const wrap = (type, str) => `<${type}>${str}</${type}>`;
+const wrap = type => str => `<${type}>${str}</${type}>`;
+
+const toLowerCase = str => str.toLowerCase();
+
+// const transform = compose(wrapInDiv, toLowerCase, trim);
+const transform = pipe(trim, toLowerCase, wrap('span'));
+
+console.log(transform(input));
