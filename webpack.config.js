2a7cc11bb0c385d703c2c1f0a30fdd2c4467bbb5
Varun Swarup <varunswarup0@gmail.com>
2020-03-25 08:36:21 +0530

    Added devToolsEnhancer & Enabled trace

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "dist"),
-    port: 9000
-  },
-  mode: "development"
+	entry: './src/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 9000
+	},
+	mode: 'development',
+	devtool: 'source-map'
 };
