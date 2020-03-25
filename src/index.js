6c843eaf61c6b1cc92b6cbc55852e7baee53afa9
Varun Swarup <varunswarup0@gmail.com>
2020-03-25 18:39:44 +0530

    Added users and Assigned bugs to users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, getUnresolvedBugs } from './store/bugs';
+import {
+	bugAdded,
+	bugResolved,
+	getUnresolvedBugs,
+	bugAssignedToUser,
+	getBugsByUser
+} from './store/bugs';
 import { projectAdded } from './store/projects';
+import { userAdded } from './store/users';
 
 const store = configureStore();
 
@@ -8,14 +15,19 @@ store.subscribe(() => {
 	console.log('Store Changed!');
 });
 
+store.dispatch(userAdded({ name: 'User 1' }));
+store.dispatch(userAdded({ name: 'User 2' }));
+
 store.dispatch(projectAdded({ name: 'Project 1' }));
 
 store.dispatch(bugAdded({ description: 'Bug 1' }));
 store.dispatch(bugAdded({ description: 'Bug 2' }));
 store.dispatch(bugAdded({ description: 'Bug 3' }));
+
 store.dispatch(bugResolved({ id: 1 }));
 
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState());
+store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
-console.log(x === y);
+const bugs = getBugsByUser(1)(store.getState());
+
+console.log(bugs);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -18,11 +18,16 @@ const slice = createSlice({
 		bugResolved: (bugs, action) => {
 			const index = bugs.findIndex(bug => bug.id === action.payload.id);
 			bugs[index].resolved = true;
+		},
+		bugAssignedToUser: (bugs, action) => {
+			const { bugId, userId } = action.payload;
+			const index = bugs.findIndex(bug => bug.id === bugId);
+			bugs[index].userId = userId;
 		}
 	}
 });
 
-export const { bugAdded, bugResolved } = slice.actions;
+export const { bugAdded, bugResolved, bugAssignedToUser } = slice.actions;
 export default slice.reducer;
 
 //Selector
@@ -31,3 +36,9 @@ export const getUnresolvedBugs = createSelector(
 	state => state.entities.projects,
 	(bugs, projects) => bugs.filter(bug => !bug.resolved)
 );
+
+export const getBugsByUser = userId =>
+	createSelector(
+		state => state.entities.bugs,
+		bugs => bugs.filter(bug => bug.userId === userId)
+	);
