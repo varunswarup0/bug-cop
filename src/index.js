9602f7a4e21be1d14055749490b80ac109ce8c17
Varun Swarup <varunswarup0@gmail.com>
2020-03-25 12:39:04 +0530

    Added createSelector

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ store.dispatch(bugAdded({ description: 'Bug 2' }));
 store.dispatch(bugAdded({ description: 'Bug 3' }));
 store.dispatch(bugResolved({ id: 1 }));
 
-const unresolvedBugs = getUnresolvedBugs(store.getState());
+const x = getUnresolvedBugs(store.getState());
+const y = getUnresolvedBugs(store.getState());
 
-console.log(unresolvedBugs);
+console.log(x === y);
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
 
 let lastId = 0;
 
@@ -25,5 +26,8 @@ export const { bugAdded, bugResolved } = slice.actions;
 export default slice.reducer;
 
 //Selector
-export const getUnresolvedBugs = state =>
-	state.entities.bugs.filter(bug => !bug.resolved);
+export const getUnresolvedBugs = createSelector(
+	state => state.entities.bugs,
+	state => state.entities.projects,
+	(bugs, projects) => bugs.filter(bug => !bug.resolved)
+);
