04ba7eb86d5d86075e69da798f833a59f08666d7
Varun Swarup <varunswarup0@gmail.com>
2020-03-27 01:54:09 +0530

    bugSlice: load, unresolved & resolvedr

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -60,7 +60,7 @@ export const loadBugs = () => (dispatch, getState) => {
 	const diffInMinutes = moment().diff(moment(lastFetch), 'minutes');
 	if (diffInMinutes < 10) return;
 
-	dispatch(
+	return dispatch(
 		apiCallBegan({
 			url,
 			onStart: bugsRequested.type,
@@ -102,7 +102,7 @@ export const assignBugToUser = (bugId, userId) =>
 export const getUnresolvedBugs = createSelector(
 	state => state.entities.bugs,
 	state => state.entities.projects,
-	(bugs, projects) => bugs.filter(bug => !bug.resolved)
+	(bugs, projects) => bugs.list.filter(bug => !bug.resolved)
 );
 
 export const getBugsByUser = userId =>
--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { addBug } from '../bugs';
+import { loadBugs, addBug, resolveBug, getUnresolvedBugs } from '../bugs';
 import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
@@ -12,7 +12,87 @@ describe('bugsSlice', () => {
 		store = configureStore();
 	});
 
-	const bugSlice = () => store.getState().entities.bugs;
+	const bugsSlice = () => store.getState().entities.bugs;
+
+	const createState = () => ({
+		entities: {
+			bugs: {
+				list: []
+			}
+		}
+	});
+
+	describe('loading bugs', () => {
+		describe('if the bugs exist in the cache', () => {
+			it('they should not be fetched from the server again.', async () => {
+				fakeAxios.onGet('/bugs').reply(200, [{ id: 1 }]);
+
+				await store.dispatch(loadBugs());
+				await store.dispatch(loadBugs());
+
+				expect(fakeAxios.history.get.length).toBe(1);
+			});
+		});
+
+		describe("if the bugs don't exist in the cache", () => {
+			it('they should be fetched from the server and put in the store', async () => {
+				fakeAxios.onGet('/bugs').reply(200, [{ id: 1 }]);
+
+				await store.dispatch(loadBugs());
+
+				expect(bugsSlice().list).toHaveLength(1);
+			});
+
+			describe('loading indicator', () => {
+				it('should be true while fetching the bugs', () => {
+					fakeAxios.onGet('/bugs').reply(() => {
+						expect(bugsSlice().loading).toBe(true);
+						return [200, [{ id: 1 }]];
+					});
+
+					store.dispatch(loadBugs());
+				});
+
+				it('should be false after the bugs are fetched', async () => {
+					fakeAxios.onGet('/bugs').reply(200, [{ id: 1 }]);
+
+					await store.dispatch(loadBugs());
+
+					expect(bugsSlice().loading).toBe(false);
+				});
+
+				it('should be false if the server returns an error', async () => {
+					fakeAxios.onGet('/bugs').reply(500);
+
+					await store.dispatch(loadBugs());
+
+					expect(bugsSlice().loading).toBe(false);
+				});
+			});
+		});
+	});
+
+	it("should mark the bug as resolved if it's saved to the server.", async () => {
+		// AAA
+		fakeAxios.onPatch('/bugs/1').reply(200, { id: 1, resolved: true });
+		fakeAxios.onPost('/bugs').reply(200, { id: 1 });
+
+		await store.dispatch(addBug({}));
+		await store.dispatch(resolveBug(1));
+
+		expect(bugsSlice().list[0].resolved).toBe(true);
+	});
+
+	it("should not mark the bug as resolved if it's not saved to the server.", async () => {
+		// AAA
+		fakeAxios.onPatch('/bugs/1').reply(500);
+		fakeAxios.onPost('/bugs').reply(200, { id: 1 });
+
+		await store.dispatch(addBug({}));
+		await store.dispatch(resolveBug(1));
+
+		expect(bugsSlice().list[0].resolved).not.toBe(true);
+	});
 
 	it("should add the bug to the store if it's saved to the server", async () => {
 		const bug = { description: 'a' };
@@ -21,7 +101,7 @@ describe('bugsSlice', () => {
 
 		await store.dispatch(addBug(bug));
 
-		expect(bugSlice().list).toContainEqual(savedBug);
+		expect(bugsSlice().list).toContainEqual(savedBug);
 	});
 
 	it("should not add the bug to the store if it's not saved to the server", async () => {
@@ -30,6 +110,21 @@ describe('bugsSlice', () => {
 
 		await store.dispatch(addBug(bug));
 
-		expect(bugSlice().list).toHaveLength(0);
+		expect(bugsSlice().list).toHaveLength(0);
+	});
+
+	describe('selectors', () => {
+		it('getUnresolvedBugs', () => {
+			const state = createState();
+			state.entities.bugs.list = [
+				{ id: 1, resolved: true },
+				{ id: 2 },
+				{ id: 3 }
+			];
+
+			const result = getUnresolvedBugs(state);
+
+			expect(result).toHaveLength(2);
+		});
 	});
 });
